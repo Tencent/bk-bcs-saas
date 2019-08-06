51dd2335c360b3b11956bdf7eb22b7271c16ca48
joelei <thezero12@hotmail.com>
2019-08-06 16:32:27 +0800

    change source name

--- bcs-app/backend/web_console/rest_api/views.py
+++ bcs-app/backend/web_console/rest_api/views.py
@@ -173,7 +173,7 @@ class WebConsoleSession(views.APIView):
         else:
             scheme = 'ws'
         bcs_api_url = bcs_api_url._replace(scheme=scheme)
-        source = request.query_params.get('source', 'base')
+        source = request.query_params.get('source', 'direct')
 
         ws_url = f'{bcs_api_url.geturl()}/web_console/projects/{project_id}/clusters/{cluster_id}/ws/?session_id={session_id}&source={source}'  # noqa
 
