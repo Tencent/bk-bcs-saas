eea14481508c75031a37ec28eef9bb868dcba298
bellkeyang <bellkeyang@tencent.com>
2019-08-08 14:32:18 +0800

    feat: sort the configmap/secret

--- bcs-app/backend/apps/resource/views.py
+++ bcs-app/backend/apps/resource/views.py
@@ -800,8 +800,11 @@ def data_handler(data, project_id):
                 RE_COMPILE.findall(info["createTime"])[:2])
         if project_id == 1 and 'data' in info:
             info["data"]["datas"] = {}
-            for key, val in ((info.get("data") or {}).get("data") or {}).items():
+            info_data = (info.get('data') or {}).get('data') or {}
+            for key, val in info_data.items():
                 info["data"]["datas"].update({key: {"content": val}})
+            info['data']['datas'] = dict(sorted(info['data']['datas'].items(), key=lambda x: x[0]))
+            info['data']['data'] = dict(sorted(info_data.items(), key=lambda x: x[0]))
             ret_data.append(info)
         else:
             ret_data.append(info)
