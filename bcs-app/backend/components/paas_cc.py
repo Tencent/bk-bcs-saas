b5e40029b79dc5f8a6bd46c64c6cfcc6468270e3
bellkeyang <bellkeyang@tencent.com>
2019-11-25 09:38:12 +0800

    feat: add area source

--- bcs-app/backend/components/paas_cc.py
+++ bcs-app/backend/components/paas_cc.py
@@ -119,9 +119,9 @@ def get_cluster_snapshot(access_token, project_id, cluster_id):
     return http_get(url, params=params)
 
 
-def get_area_list(access_token):
+def get_area_list(access_token, source=''):
     url = f'{CC_HOST}/areas/'
-    params = {"access_token": access_token}
+    params = {"access_token": access_token, "source": source}
     return http_get(url, params=params)
 
 
