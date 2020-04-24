338dd38c34a84b68e12db9f7bbbaa8f36100d4b0
joelei <thezero12@hotmail.com>
2020-04-24 14:19:36 +0800

    web-console connect apiserver in cluster

--- bcs-app/backend/web_console/pod_life_cycle.py
+++ bcs-app/backend/web_console/pod_life_cycle.py
@@ -15,6 +15,7 @@ import json
 import logging
 import shlex
 import time
+import base64
 from concurrent.futures import ThreadPoolExecutor
 
 import yaml
@@ -50,12 +51,9 @@ class PodLifeCycle:
     def heartbeat(cls, name):
         """定时上报存活, 清理时需要使用
         """
-        logger.debug('heartbeat: %s', name)
+        logger.debug("heartbeat: %s", name)
 
-        result = rd_client.zadd(
-            constants.WEB_CONSOLE_HEARTBEAT_KEY,
-            name,
-            time.time())
+        result = rd_client.zadd(constants.WEB_CONSOLE_HEARTBEAT_KEY, name, time.time())
 
         return result
 
@@ -64,25 +62,25 @@ class PodLifeCycle:
         """
         now = time.time()
         start = now - constants.USER_POD_EXPIRE_TIME
-        expired_pods = rd_client.zremrangebyscore(constants.WEB_CONSOLE_HEARTBEAT_KEY, '-inf', start)
+        expired_pods = rd_client.zremrangebyscore(constants.WEB_CONSOLE_HEARTBEAT_KEY, "-inf", start)
         actived_pods = rd_client.zrange(constants.WEB_CONSOLE_HEARTBEAT_KEY, 0, -1, withscores=True)
         actived_pods = [(smart_text(i[0]), now - i[1]) for i in actived_pods]
 
         pods = [i[0] for i in actived_pods]
 
-        idle_pods_msg = ', '.join('{pod[0]}={pod[1]:.2f}s'.format(pod=pod) for pod in actived_pods)
-        logger.info('remove expired_pods, %s, get actived pods: %s', expired_pods, idle_pods_msg)
+        idle_pods_msg = ", ".join("{pod[0]}={pod[1]:.2f}s".format(pod=pod) for pod in actived_pods)
+        logger.info("remove expired_pods, %s, get actived pods: %s", expired_pods, idle_pods_msg)
 
         return pods
 
-    @redis_lock('pod_life_cycle.clean_user_pod', constants.CLEAN_USER_POD_INTERVAL, shift=constants.LOCK_SHIFT)
+    @redis_lock("pod_life_cycle.clean_user_pod", constants.CLEAN_USER_POD_INTERVAL, shift=constants.LOCK_SHIFT)
     @run_on_executor
     def clean_user_pod(self):
-        logger.debug('start clean user pod')
+        logger.debug("start clean user pod")
 
         try:
             self._clean_user_pod()
-            logger.debug('clean user pod success')
+            logger.debug("clean user pod success")
         except Exception as error:
             logger.error("clean user pod error: %s", error)
 
@@ -96,13 +94,13 @@ class PodLifeCycle:
                 pod_list = v1.list_namespaced_pod(namespace=constants.NAMESPACE)
                 self._clean_user_pod_by_cluster(v1, pod_list, alive_pods)
             except Exception as error:
-                logger.info('clean %s pod not success, %s', cluster_id, error)
+                logger.info("clean %s pod not success, %s", cluster_id, error)
 
     def _clean_user_pod_by_cluster(self, v1, pod_list, alive_pods):
         min_expire_time = time.time() - constants.USER_POD_EXPIRE_TIME
 
         for pod in pod_list.items:
-            if pod.status.phase == 'Pending':
+            if pod.status.phase == "Pending":
                 continue
 
             # 小于一个周期的pod不清理
@@ -112,7 +110,9 @@ class PodLifeCycle:
                 pod_create_time = None
 
             if pod_create_time and pod_create_time > min_expire_time:
-                logger.info('pod %s exist time %s > %s, just ignore', pod.metadata.name, pod_create_time, min_expire_time)
+                logger.info(
+                    "pod %s exist time %s > %s, just ignore", pod.metadata.name, pod_create_time, min_expire_time
+                )
                 continue
 
             # 有心跳上报的pod不清理
@@ -120,24 +120,21 @@ class PodLifeCycle:
                 continue
 
             v1.delete_namespaced_pod(
-                name=pod.metadata.name,
-                namespace=constants.NAMESPACE,
-                body=client.V1DeleteOptions())
-            logger.info('delete pod %s', pod.metadata.name)
+                name=pod.metadata.name, namespace=constants.NAMESPACE, body=client.V1DeleteOptions()
+            )
+            logger.info("delete pod %s", pod.metadata.name)
 
             for volume in pod.spec.volumes:
-                cm = getattr(volume, 'config_map', None)
+                cm = getattr(volume, "config_map", None)
                 if not cm:
                     continue
 
                 cm_name = cm.name
 
                 v1.delete_namespaced_config_map(
-                    name=cm_name,
-                    namespace=constants.NAMESPACE,
-                    body=client.V1DeleteOptions()
+                    name=cm_name, namespace=constants.NAMESPACE, body=client.V1DeleteOptions()
                 )
-                logger.info('delete configmap %s', cm_name)
+                logger.info("delete configmap %s", cm_name)
 
     def start(self):
         self.scheduler = PeriodicCallback(self.clean_user_pod, constants.CLEAN_USER_POD_INTERVAL * 1000)
@@ -145,7 +142,7 @@ class PodLifeCycle:
 
 
 class K8SClient(object):
-    CACHE_KEY_PREFIX = 'K8S:USER_TOKEN'
+    CACHE_KEY_PREFIX = "K8S:USER_TOKEN"
 
     def __init__(self, ctx=None):
         self.ctx = ctx
@@ -157,12 +154,12 @@ class K8SClient(object):
         aConfiguration.host = server_address
         aConfiguration.api_key = {"authorization": f"Bearer {user_token}"}
         aApiClient = client.ApiClient(aConfiguration)
-        logger.info("use %s client, %s, %s", ctx['mode'], aConfiguration.host, aConfiguration.api_key)
+        logger.info("use %s client, %s, %s", ctx["mode"], aConfiguration.host, aConfiguration.api_key)
         return aApiClient
 
     @classmethod
     def iter_client(cls):
-        for key in rd_client.scan_iter(f'{cls.CACHE_KEY_PREFIX}:*'):
+        for key in rd_client.scan_iter(f"{cls.CACHE_KEY_PREFIX}:*"):
             key = smart_text(key)
             data = rd_client.get(key)
             try:
@@ -172,25 +169,29 @@ class K8SClient(object):
                 continue
 
             k8s_client = cls.get_api_client(
-                ctx['admin_server_address'], ctx['admin_cluster_identifier'], ctx['admin_user_token'], ctx)
+                ctx["admin_server_address"], ctx["admin_cluster_identifier"], ctx["admin_user_token"], ctx
+            )
             v1 = client.CoreV1Api(k8s_client)
-            yield (v1, ctx['cluster_id'])
+            yield (v1, ctx["cluster_id"])
 
     def get_client(self, ctx):
         # 每次保存最新的user_token， 12小时过期
-        source_cluster_id = ctx['source_cluster_id'].lower()
-        data = {'admin_user_token': ctx['admin_user_token'],
-                'admin_cluster_identifier': ctx['admin_cluster_identifier'],
-                'cluster_id': source_cluster_id,
-                'admin_server_address': ctx['admin_server_address'],
-                'mode': ctx['mode']}
+        source_cluster_id = ctx["source_cluster_id"].lower()
+        data = {
+            "admin_user_token": ctx["admin_user_token"],
+            "admin_cluster_identifier": ctx["admin_cluster_identifier"],
+            "cluster_id": source_cluster_id,
+            "admin_server_address": ctx["admin_server_address"],
+            "mode": ctx["mode"],
+        }
 
-        if ctx.get('should_cache_ctx') is True:
+        if ctx.get("should_cache_ctx") is True:
             name = f"{source_cluster_id}-u{ctx['username_slug']}"
             CACHE_KEY = f"{self.CACHE_KEY_PREFIX}:{name}"
             rd_client.set(CACHE_KEY, json.dumps(data), ex=constants.USER_CTX_EXPIRE_TIME)
         k8s_client = self.get_api_client(
-            ctx['admin_server_address'], ctx['admin_cluster_identifier'], ctx['admin_user_token'], ctx)
+            ctx["admin_server_address"], ctx["admin_cluster_identifier"], ctx["admin_user_token"], ctx
+        )
         return k8s_client
 
     @property
@@ -199,16 +200,28 @@ class K8SClient(object):
         v1_client = client.CoreV1Api(k8s_client)
         return v1_client
 
+    @property
+    def rbac(self):
+        k8s_client = self.get_client(self.ctx)
+        rbac = client.RbacAuthorizationV1Api(k8s_client)
+        return rbac
+
     def exec_command(self, command: str) -> str:
         """执行命令，返回输出结果
         """
         command = shlex.split(command)
-        resp = stream(self.v1.connect_get_namespaced_pod_exec, self.ctx['user_pod_name'], self.ctx['namespace'],
-                      command=command,
-                      container=self.ctx['container_name'],
-                      stderr=True, stdin=False,
-                      stdout=True, tty=False,
-                      _preload_content=True)
+        resp = stream(
+            self.v1.connect_get_namespaced_pod_exec,
+            self.ctx["user_pod_name"],
+            self.ctx["namespace"],
+            command=command,
+            container=self.ctx["container_name"],
+            stderr=True,
+            stdin=False,
+            stdout=True,
+            tty=False,
+            _preload_content=True,
+        )
         return resp
 
 
@@ -224,7 +237,7 @@ def wait_user_pod_ready(ctx, name):
     while total_sleep < wait_timeout:
         try:
             pod = k8s_client.v1.read_namespaced_pod(name, namespace=constants.NAMESPACE)
-            if pod.status.phase == 'Running':
+            if pod.status.phase == "Running":
                 return pod
         except Exception as error:
             # 错误一次返回
@@ -240,47 +253,82 @@ def wait_user_pod_ready(ctx, name):
     raise PodLifeError(_("申请pod资源超时，请稍后再试{}").format(settings.COMMON_EXCEPTION_MSG))
 
 
+def get_service_account_token(k8s_client):
+    """获取web-console token
+    """
+    if settings.REGION not in ["ee", "ce"]:
+        return
+
+    token_prefix = f"{constants.NAMESPACE}-token"
+    for item in k8s_client.v1.list_namespaced_secret(constants.NAMESPACE).items:
+        if not item.metadata.name.startswith(token_prefix):
+            continue
+
+        return base64.b64decode(item.data["token"])
+
+
+def create_service_account_rbac(k8s_client, ctx):
+    """创建serviceAccount, 绑定Role
+    """
+    if settings.REGION not in ["ee", "ce"]:
+        return
+
+    service_account_body = yaml.load(render_to_string("conf_tpl/service_account.yaml", ctx))
+    service_account_rolebind_body = yaml.load(render_to_string("conf_tpl/service_account_rolebind.yaml", ctx))
+
+    k8s_client.v1.create_namespaced_service_account(constants.NAMESPACE, service_account_body)
+    k8s_client.rbac.create_cluster_role_binding(service_account_rolebind_body)
+
+
 def ensure_namespace(ctx):
     """创建命名空间
     """
     k8s_client = K8SClient(ctx)
 
+    def _add_token_to_ctx(k8s_client, ctx):
+        # 获取service_account_token
+        try:
+            ctx["service_account_token"] = get_service_account_token(k8s_client)
+        except Exception as error:
+            logger.error("get service_account_token error, %s", error)
+            ctx["service_account_token"] = None
+
     try:
         ns = k8s_client.v1.read_namespace(name=constants.NAMESPACE)
+        _add_token_to_ctx(k8s_client, ctx)
         return ns
     except ApiException as error:
         if error.status == 404:
-            body = yaml.load(render_to_string('conf_tpl/namespace.yaml', ctx))
+            body = yaml.load(render_to_string("conf_tpl/namespace.yaml", ctx))
             try:
                 ns = k8s_client.v1.create_namespace(body=body)
+                create_service_account_rbac(k8s_client, ctx)
+                _add_token_to_ctx(k8s_client, ctx)
                 return ns
             except ApiException as error:
-                logger.exception('create namespace error: %s', error)
+                logger.exception("create namespace error: %s", error)
                 raise error
 
 
 def ensure_configmap(ctx):
     """创建configmap
     """
-    name = 'kube-config-%s-u%s' % (ctx['source_cluster_id'], ctx['username_slug'])
+    name = "kube-config-%s-u%s" % (ctx["source_cluster_id"], ctx["username_slug"])
     name = name.lower()
 
     k8s_client = K8SClient(ctx)
-
     try:
         cm = k8s_client.v1.read_namespaced_config_map(name, namespace=constants.NAMESPACE)
         return cm
     except ApiException as error:
         # 不存在，则创建
         if error.status == 404:
-            body = yaml.load(render_to_string('conf_tpl/configmap.yaml', ctx))
+            body = yaml.load(render_to_string("conf_tpl/configmap.yaml", ctx))
             try:
-                cm = k8s_client.v1.create_namespaced_config_map(
-                    body=body, namespace=constants.NAMESPACE
-                )
+                cm = k8s_client.v1.create_namespaced_config_map(body=body, namespace=constants.NAMESPACE)
                 return cm
             except ApiException as error:
-                logger.exception('create config_map error: %s', error)
+                logger.exception("create config_map error: %s", error)
                 raise error
         raise error
 
@@ -288,25 +336,25 @@ def ensure_configmap(ctx):
 def ensure_pod(ctx):
     """创建configmap
     """
-    name = 'kubectld-%s-u%s' % (ctx['source_cluster_id'], ctx['username_slug'])
+    name = "kubectld-%s-u%s" % (ctx["source_cluster_id"], ctx["username_slug"])
     name = name.lower()
 
     k8s_client = K8SClient(ctx)
 
     try:
         pod = k8s_client.v1.read_namespaced_pod(name, namespace=constants.NAMESPACE)
-        if pod.status.phase != 'Running':
+        if pod.status.phase != "Running":
             raise PodLifeError(_("pod不是Running状态，请稍后再试{}").format(settings.COMMON_EXCEPTION_MSG))
         return pod
     except ApiException as error:
         # 不存在，则创建
         if error.status == 404:
             # 添加时间戳
-            ctx['LABEL_WEB_CONSOLE_CREATE_TIMESTAMP'] = constants.LABEL_WEB_CONSOLE_CREATE_TIMESTAMP
-            ctx['create_timestamp'] = int(time.time())
-            body = yaml.load(render_to_string('conf_tpl/pod.yaml', ctx))
+            ctx["LABEL_WEB_CONSOLE_CREATE_TIMESTAMP"] = constants.LABEL_WEB_CONSOLE_CREATE_TIMESTAMP
+            ctx["create_timestamp"] = int(time.time())
+            body = yaml.load(render_to_string("conf_tpl/pod.yaml", ctx))
             # 添加环境特有变量
-            body['spec'].update(ctx['pod_spec'])
+            body["spec"].update(ctx["pod_spec"])
             try:
                 k8s_client.v1.create_namespaced_pod(body=body, namespace=constants.NAMESPACE)
                 pod = wait_user_pod_ready(ctx, name)
