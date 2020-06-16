6002a462ad0aa81ab18d122c3954ec8f1aa981f1
joelei <thezero12@hotmail.com>
2020-06-16 16:43:20 +0800

    add open session

--- bcs-app/backend/web_console/rest_api/views.py
+++ bcs-app/backend/web_console/rest_api/views.py
@@ -18,12 +18,11 @@ import uuid
 from urllib.parse import urlparse
 
 from django.conf import settings
-from rest_framework import views
-from rest_framework.renderers import BrowsableAPIRenderer
 from django.utils.translation import ugettext_lazy as _
 
 from backend.accounts import bcs_perm
 from backend.apps.constants import ProjectKind
+from backend.components import paas_auth
 from backend.components.bcs.k8s import K8SClient
 from backend.components.bcs.mesos import MesosClient
 from backend.utils.cache import rd_client
@@ -33,10 +32,12 @@ from backend.utils.response import BKAPIResponse
 from backend.web_console import constants, pod_life_cycle
 from backend.web_console.bcs_client import k8s, mesos
 from backend.web_console.utils import get_kubectld_version
+from rest_framework import views
+from rest_framework.renderers import BrowsableAPIRenderer
 
+from ..session import session_mgr
 from . import utils
 from .serializers import K8SWebConsoleSLZ, MesosWebConsoleSLZ
-from ..session import session_mgr
 
 logger = logging.getLogger(__name__)
 
@@ -49,7 +50,7 @@ class WebConsoleSession(views.APIView):
         """获取docker监控信息
         """
         client = K8SClient(request.user.token.access_token, project_id, cluster_id, None)
-        slz = K8SWebConsoleSLZ(data=request.query_params, context={'client': client})
+        slz = K8SWebConsoleSLZ(data=request.query_params, context={"client": client})
         slz.is_valid(raise_exception=True)
 
         try:
@@ -61,47 +62,48 @@ class WebConsoleSession(views.APIView):
             utils.activity_log(project_id, self.cluster_name, request.user.username, False, message)
             # 返回前端消息
             raise error_codes.APIError(
-                _("{}，请检查 Deployment【kube-system/bcs-agent】是否正常{}").format(
-                    message, settings.COMMON_EXCEPTION_MSG
-                ))
+                _("{}，请检查 Deployment【kube-system/bcs-agent】是否正常{}").format(message, settings.COMMON_EXCEPTION_MSG)
+            )
 
         # kubectl版本区别
         kubectld_version = get_kubectld_version(client.version)
 
-        container_id = slz.validated_data.get('container_id')
+        container_id = slz.validated_data.get("container_id")
         if container_id:
-            bcs_context['mode'] = k8s.ContainerDirectClient.MODE
-            bcs_context['user_pod_name'] = slz.validated_data['pod_name']
+            bcs_context["mode"] = k8s.ContainerDirectClient.MODE
+            bcs_context["user_pod_name"] = slz.validated_data["pod_name"]
             bcs_context.update(slz.validated_data)
 
         else:
             bcs_context = utils.get_k8s_admin_context(client, bcs_context, settings.WEB_CONSOLE_MODE)
 
-            ctx = {'username': self.request.user.username,
-                   'settings': settings,
-                   'kubectld_version': kubectld_version,
-                   'namespace': constants.NAMESPACE,
-                   'pod_spec': utils.get_k8s_pod_spec(client),
-                   'username_slug': utils.get_username_slug(self.request.user.username),
-                   # 缓存ctx， 清理使用
-                   'should_cache_ctx': True}
+            ctx = {
+                "username": self.request.user.username,
+                "settings": settings,
+                "kubectld_version": kubectld_version,
+                "namespace": constants.NAMESPACE,
+                "pod_spec": utils.get_k8s_pod_spec(client),
+                "username_slug": utils.get_username_slug(self.request.user.username),
+                # 缓存ctx， 清理使用
+                "should_cache_ctx": True,
+            }
             ctx.update(bcs_context)
             try:
                 pod_life_cycle.ensure_namespace(ctx)
                 configmap = pod_life_cycle.ensure_configmap(ctx)
-                logger.debug('get configmap %s', configmap)
+                logger.debug("get configmap %s", configmap)
                 pod = pod_life_cycle.ensure_pod(ctx)
-                logger.debug('get pod %s', pod)
+                logger.debug("get pod %s", pod)
             except pod_life_cycle.PodLifeError as error:
                 logger.error("kubetctl apply error: %s", error)
-                utils.activity_log(project_id, self.cluster_name, request.user.username, False, '%s' % error)
-                raise error_codes.APIError('%s' % error)
+                utils.activity_log(project_id, self.cluster_name, request.user.username, False, "%s" % error)
+                raise error_codes.APIError("%s" % error)
             except Exception as error:
                 logger.exception("kubetctl apply error: %s", error)
                 utils.activity_log(project_id, self.cluster_name, request.user.username, False, "申请pod资源失败")
                 raise error_codes.APIError(_("申请pod资源失败，请稍后再试{}").format(settings.COMMON_EXCEPTION_MSG))
 
-            bcs_context['user_pod_name'] = pod.metadata.name
+            bcs_context["user_pod_name"] = pod.metadata.name
 
         return bcs_context
 
@@ -109,34 +111,36 @@ class WebConsoleSession(views.APIView):
         """获取mesos context
         """
         client = MesosClient(request.user.token.access_token, project_id, cluster_id, None)
-        slz = MesosWebConsoleSLZ(data=request.query_params, context={'client': client})
+        slz = MesosWebConsoleSLZ(data=request.query_params, context={"client": client})
         slz.is_valid(raise_exception=True)
 
         context = {
-            'short_container_id': slz.validated_data['container_id'][:12],
-            'project_kind': request.project.kind,
-            'server_address': client._bcs_server_host,
-            'user_pod_name': slz.validated_data['container_name'],
+            "short_container_id": slz.validated_data["container_id"][:12],
+            "project_kind": request.project.kind,
+            "server_address": client._bcs_server_host,
+            "user_pod_name": slz.validated_data["container_name"],
         }
         context.update(slz.validated_data)
 
-        exec_id = client.get_container_exec_id(context['host_ip'], context['short_container_id'])
+        exec_id = client.get_container_exec_id(context["host_ip"], context["short_container_id"])
         if not exec_id:
             utils.activity_log(
-                project_id, self.cluster_name, request.user.username, False, f'连接{context["user_pod_name"]}失败')
+                project_id, self.cluster_name, request.user.username, False, f'连接{context["user_pod_name"]}失败'
+            )
 
-            raise error_codes.APIError(_('连接 {} 失败，请检查容器状态是否正常{}').format(
-                context["user_pod_name"], settings.COMMON_EXCEPTION_MSG))
-        context['exec_id'] = exec_id
-        context['mode'] = mesos.ContainerDirectClient.MODE
+            raise error_codes.APIError(
+                _("连接 {} 失败，请检查容器状态是否正常{}").format(context["user_pod_name"], settings.COMMON_EXCEPTION_MSG)
+            )
+        context["exec_id"] = exec_id
+        context["mode"] = mesos.ContainerDirectClient.MODE
 
         client_context = {
-            'access_token': request.user.token.access_token,
-            'project_id': project_id,
-            'cluster_id': cluster_id,
-            'env': client._bcs_server_stag
+            "access_token": request.user.token.access_token,
+            "project_id": project_id,
+            "cluster_id": cluster_id,
+            "env": client._bcs_server_stag,
         }
-        context['client_context'] = client_context
+        context["client_context"] = client_context
         return context
 
     def get(self, request, project_id, cluster_id):
@@ -150,19 +154,20 @@ class WebConsoleSession(views.APIView):
             raise error
 
         # resource_name字段长度限制32位
-        self.cluster_name = perm.res['name'][:32]
+        self.cluster_name = perm.res["name"][:32]
 
         # 获取web-console context信息
         if request.project.kind == ProjectKind.MESOS.value:
             # 添加白名单控制
             from .views_bk import ensure_mesos_wlist
+
             ensure_mesos_wlist(project_id, cluster_id, request.user.username)
             context = self.get_mesos_context(request, project_id, cluster_id)
         else:
             context = self.get_k8s_context(request, project_id, cluster_id)
 
-        context['username'] = request.user.username
-        context.setdefault('namespace', constants.NAMESPACE)
+        context["username"] = request.user.username
+        context.setdefault("namespace", constants.NAMESPACE)
         logger.info(context)
 
         session = session_mgr.create(project_id, cluster_id)
@@ -170,20 +175,91 @@ class WebConsoleSession(views.APIView):
 
         # 替换http为ws地址
         bcs_api_url = urlparse(settings.DEVOPS_BCS_API_URL)
-        if bcs_api_url.scheme == 'https':
-            scheme = 'wss'
+        if bcs_api_url.scheme == "https":
+            scheme = "wss"
         else:
-            scheme = 'ws'
+            scheme = "ws"
         bcs_api_url = bcs_api_url._replace(scheme=scheme)
         # 连接ws的来源方式, 有容器直连(direct)和多tab管理(mgr)
-        source = request.query_params.get('source', 'direct')
+        source = request.query_params.get("source", "direct")
 
-        ws_url = f'{bcs_api_url.geturl()}/web_console/projects/{project_id}/clusters/{cluster_id}/ws/?session_id={session_id}&source={source}'  # noqa
+        ws_url = f"{bcs_api_url.geturl()}/web_console/projects/{project_id}/clusters/{cluster_id}/ws/?session_id={session_id}&source={source}"  # noqa
 
-        data = {
-            'session_id': session_id,
-            'ws_url': ws_url
-        }
+        data = {"session_id": session_id, "ws_url": ws_url}
         utils.activity_log(project_id, self.cluster_name, request.user.username, True)
+
+        return BKAPIResponse(data, message=_("获取session成功"))
+
+
+class OpenSession(views.APIView):
+    renderer_classes = (BKAPIRenderer, BrowsableAPIRenderer)
+    permission_classes = ()
+    authentication_classes = ()
+
+    def get(self, request):
+        """校验session_id，同时换取ws_url
+        """
+        session_id = request.GET.get("session_id")
+        if not session_id:
+            raise error_codes.APIError(_("session_id不能为空"))
+
+        session = session_mgr.create("", "")
+        context = session.get(session_id)
+        if not context:
+            raise error_codes.APIError(_("session_id不合法或已经过期"))
+
+        ws_session = session_mgr.create(context["project_id"], context["cluster_id"])
+        ws_session_id = ws_session.set(context)
+
+        bcs_api_url = urlparse(settings.DEVOPS_BCS_API_URL)
+        if bcs_api_url.scheme == "https":
+            scheme = "wss"
+        else:
+            scheme = "ws"
+        bcs_api_url = bcs_api_url._replace(scheme=scheme)
+
+        ws_url = f"{bcs_api_url.geturl()}/web_console/projects/{context['project_id']}/clusters/{context['cluster_id']}/ws/?session_id={ws_session_id}&source=direct"  # noqa
+
+        data = {"session_id": session_id, "ws_url": ws_url}
+        return BKAPIResponse(data, message=_("获取ws session成功"))
+
+    def get_k8s_context(self, request, project_id_or_code, cluster_id):
+        """获取docker监控信息
+        """
+        access_token = paas_auth.get_access_token()
+        client = K8SClient(access_token["access_token"], project_id_or_code, cluster_id, None)
+        slz = K8SWebConsoleSLZ(data=request.data, context={"client": client})
+        slz.is_valid(raise_exception=True)
+
+        try:
+            bcs_context = utils.get_k8s_cluster_context(client, project_id_or_code, cluster_id)
+        except Exception as error:
+            logger.exception("get access cluster context failed: %s", error)
+            message = _("获取集群{}【{}】WebConsole session 信息失败").format(cluster_id, cluster_id)
+            # 返回前端消息
+            raise error_codes.APIError(message)
+
+        bcs_context["mode"] = k8s.ContainerDirectClient.MODE
+        bcs_context["user_pod_name"] = slz.validated_data["pod_name"]
+        bcs_context.update(slz.validated_data)
+
+        return bcs_context
+
+    def post(self, request, project_id_or_code, cluster_id):
+        """获取session信息
+        """
+        context = self.get_k8s_context(request, project_id_or_code, cluster_id)
+
+        context["username"] = request.user.username
+        context.setdefault("namespace", constants.NAMESPACE)
+
+        logger.info(context)
+
+        session = session_mgr.create("", "")
+        context["project_id"] = project_id_or_code
+        context["cluster_id"] = cluster_id
+        session_id = session.set(context)
+
+        data = {"session_id": session_id}
 
         return BKAPIResponse(data, message=_("获取session成功"))
