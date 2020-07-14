b88ae9a1e4eabea48433ee4e4fe7ba7c8cebbce7
bellkeyang <bellkeyang@tencent.com>
2020-07-14 14:05:48 +0800

    feat: add increament chart api

--- bcs-app/backend/bcs_k8s/helm/utils/repo_bk.py
+++ bcs-app/backend/bcs_k8s/helm/utils/repo_bk.py
@@ -11,3 +11,5 @@
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 #
+def get_incremental_charts_and_hash_value(url_prefix, username, password, start_time):
+    return None, None
