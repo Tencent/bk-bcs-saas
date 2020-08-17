2d749d07c8e690c81f2678494472d0917008401e
ielgnaw <wuji0223@gmail.com>
2020-08-17 19:50:11 +0800

    应用模块调整 (#577)

--- bcs-app/frontend/src/common/lang.js
+++ bcs-app/frontend/src/common/lang.js
@@ -1233,6 +1233,9 @@ export default {
     '调度引擎': ['COES'],
     '输入集群名称搜索': ['Enter Cluster to Search'],
     '输入命名空间搜索': ['Enter Namespace to Search'],
+    '请选择CRD': ['Please Select CRD'],
+    '输入CRD搜索': ['Enter CRD to Search'],
+    '输入名称搜索': ['Enter Name to Search'],
     // 变量的使用 $t('test', { vari1: 1, vari2: 2 })
     'test': ['{vari1} ---english--- {vari2}']
 }
