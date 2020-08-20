f7a8f9b72bde86d0925b840f5500f011fdb920f5
ielgnaw <wuji0223@gmail.com>
2020-08-20 17:35:28 +0800

    集群, 节点 metric 调整;应用调整

--- bcs-app/frontend/src/common/lang.js
+++ bcs-app/frontend/src/common/lang.js
@@ -1236,6 +1236,13 @@ export default {
     '请选择CRD': ['Please Select CRD'],
     '输入CRD搜索': ['Enter CRD to Search'],
     '输入名称搜索': ['Enter Name to Search'],
+    'IP数量(个)': ['IPs'],
+    '可用IP(个)': ['IPs'],
+    'CPU数量(核)': ['CPUs'],
+    '可用CPU(核)': ['CPUs'],
+    '内存数量(GB)': ['Memory(GB)'],
+    '可用内存(GB)': ['Memory(GB)'],
+    '确定删除？': ['Confirm to delete?'],
     // 变量的使用 $t('test', { vari1: 1, vari2: 2 })
     'test': ['{vari1} ---english--- {vari2}']
 }
