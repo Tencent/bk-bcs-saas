5bb7653bcdc38d51aeb2bed625db993c13b7658f
bellkeyang <bellkeyang@tencent.com>
2020-09-22 11:29:30 +0800

    feat: add node status filter

--- bcs-app/backend/apps/cluster/serializers.py
+++ bcs-app/backend/apps/cluster/serializers.py
@@ -249,7 +249,8 @@ class ListNodeSLZ(serializers.Serializer):
     with_containers = serializers.BooleanField(required=False)
     ordering = serializers.ChoiceField(choices=list(get_order_choices()), required=False)
     labels = serializers.ListField(required=False)
-    status = serializers.CharField(required=False)
+    status_list = serializers.ListField(default=[])
+
 
 class NodeSLZ(serializers.Serializer):
     res_id = serializers.CharField(required=True)
