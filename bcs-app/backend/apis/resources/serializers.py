af81f0951e18e15966561475910732558325f541
abner <bellkeyang@tencent.com>
2020-10-27 16:55:44 +0800

    fix: fix auth for apis (#652)

--- bcs-app/backend/apis/resources/serializers.py
+++ bcs-app/backend/apis/resources/serializers.py
@@ -12,8 +12,36 @@
 # specific language governing permissions and limitations under the License.
 #
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from backend.apps.variable.models import Variable
+from backend.apps.variable.serializers import RE_KEY
+
+class VariablesSLZ(serializers.Serializer):
+    id = serializers.IntegerField(required=False)
+    key = serializers.RegexField(RE_KEY, max_length=64)
+    value = serializers.CharField(default="")
 
 
 class CreateNamespaceParamsSLZ(serializers.Serializer):
     name = serializers.RegexField(
         r'[a-z0-9]([-a-z0-9]*[a-z0-9])?', min_length=2, max_length=63)
+    variables = serializers.ListField(child=VariablesSLZ(), default=[])
+
+    def validate_variables(self, variables):
+        if not variables:
+            return variables
+        project_id = self.context["project_id"]
+        # ns_vars 格式 [{"name": "", "key": "", "value": ""}]
+        var_key_list = [var["key"] for var in variables]
+        var_qs = Variable.objects.filter(project_id=project_id, key__in=var_key_list)
+        key_id_map = {var.key: var.id for var in var_qs}
+        # 判断key是否存在
+        not_exist_keys = set(var_key_list) - set(key_id_map.keys())
+        if not_exist_keys:
+            raise ValidationError(f"KEY: {','.join(not_exist_keys)} not found")
+        # 添加对应的
+        for var in variables:
+            var["id"] = key_id_map[var["key"]]
+
+        return variables
