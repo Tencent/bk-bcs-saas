1783544f475b9b6e86556d6293be8eb092ffcdec
abner <bellkeyang@tencent.com>
2020-12-07 17:33:20 +0800

    feat: update k8s query api (#707)

--- bcs-app/backend/components/bcs/resources/replicaset.py
+++ bcs-app/backend/components/bcs/resources/replicaset.py
@@ -18,10 +18,13 @@ import logging
 
 from kubernetes import client
 from kubernetes.client.rest import ApiException
+from django.utils.translation import ugettext_lazy as _
+
+from backend.utils.basic import getitems
+from backend.utils.error_codes import error_codes
 
 from .api_response import response
 from .resource import Resource
-from backend.utils.basic import getitems
 
 logger = logging.getLogger(__name__)
 
@@ -29,9 +32,12 @@ logger = logging.getLogger(__name__)
 class ReplicaSet(Resource):
 
     @property
-    def api_class(self):
-        # NOTE: rs 需要支持到apps/v1beta2
-        return "AppsV1beta2Api"
+    def api_versions(self):
+        resp = client.AppsApi(self.api_client).get_api_group()
+        return [
+            f"{''.join([i.capitalize() for i in ver.group_version.split('/')])}Api"
+            for ver in resp.versions
+        ]
 
     def _parse_owner_ref_name(self, params):
         owner_ref_name = None
@@ -44,14 +50,23 @@ class ReplicaSet(Resource):
             owner_ref_name = re.split(r',|;', owner_ref_name)
         return owner_ref_name
 
+    def _request_api(self, func_name, *args, **kwargs):
+        for api_version in self.api_versions:
+            try:
+                api_instance = getattr(client, api_version)(self.api_client)
+                return getattr(api_instance, func_name)(*args, **kwargs)
+            except Exception as e:
+                logger.error("call %s error, apiversion: %s, error: %s", func_name, api_version, e)
+        raise error_codes.APIError(_("查询RS失败：K8S SDK中未查询到合适的版本"))
+
     @response(format_data=False)
     def get_replicaset(self, params):
         owner_ref_name = self._parse_owner_ref_name(params)
 
         if params.get('namespace'):
-            resp = self.api_instance.list_namespaced_replica_set(params['namespace'])
+            resp = self._request_api("list_namespaced_replica_set", params["namespace"])
         else:
-            resp = self.api_instance.list_replica_set_for_all_namespaces()
+            resp = self._request_api("list_replica_set_for_all_namespaces")
 
         if not owner_ref_name:
             return [
