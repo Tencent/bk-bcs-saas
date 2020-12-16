bde0caf858cb6166d1495a95fe9264ce1b8cd16b
jamesgetx <gejun.coolfriend@gmail.com>
2020-12-16 13:14:10 +0800

    refactor: add do_install_or_upgrade method in KubeHelmClient

--- bcs-app/backend/tests/helm/test_kube_helm_client.py
+++ bcs-app/backend/tests/helm/test_kube_helm_client.py
@@ -1,28 +1,41 @@
 # -*- coding: utf-8 -*-
-from unittest.mock import MagicMock
-
 from django.conf import settings
 
 from backend.bcs_k8s.kubehelm.helm import KubeHelmClient
 
-RELEASE_NAME = "bcs-saas"
-NAMESPACE = "test"
-CHART_URL = "http://127.0.0.1:8080/charts/bcs-saas-0.1.0.tgz"
-OPTIONS = {"--values": ["bcs.yaml", "bcs-saas.yaml"], "--username": "admin", "--password": "admin", "--atomic": None}
-
 settings.HELM3_BIN = "/bin/helm3"
 
 
-def mock_run_command_with_retry(max_retries, *args, **kwargs):
-    expected_command = (
-        f"/bin/helm3 install {RELEASE_NAME} --namespace {NAMESPACE} {CHART_URL} "
-        f"--values bcs.yaml --values bcs-saas.yaml --username admin --password admin --atomic"
-    )
-    assert " ".join(kwargs.get("cmd_args")) == expected_command
-    return None, None
+def test_do_install(mock_run_command_with_retry):
+    release_name = "test-example"
+    namespace = "test"
+    chart_url = "http://repo.example.com/charts/example-0.1.0.tgz"
+    options = {
+        "--values": ["bcs.yaml", "bcs-saas.yaml"],
+        "--username": "admin",
+        "--password": "admin",
+        "--atomic": None,
+    }
 
-
-def test_do_install():
-    KubeHelmClient._run_command_with_retry = MagicMock(side_effect=mock_run_command_with_retry)
     client = KubeHelmClient()
-    client.do_install_or_upgrade("install", RELEASE_NAME, NAMESPACE, CHART_URL, options=OPTIONS)
+    client.do_install_or_upgrade("install", release_name, namespace, chart_url, options=options)
+    mock_run_command_with_retry.assert_called_with(
+        max_retries=0,
+        cmd_args=[
+            settings.HELM3_BIN,
+            "install",
+            release_name,
+            "--namespace",
+            namespace,
+            chart_url,
+            "--values",
+            "bcs.yaml",
+            "--values",
+            "bcs-saas.yaml",
+            "--username",
+            "admin",
+            "--password",
+            "admin",
+            "--atomic",
+        ],
+    )
