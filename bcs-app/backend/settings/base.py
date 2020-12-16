aa319f021dbade491235a1a870999108ad8f8f8a
bellkeyang <bellkeyang@tencent.com>
2020-12-16 16:48:28 +0800

    feat: delete setting config

--- bcs-app/backend/settings/base.py
+++ bcs-app/backend/settings/base.py
@@ -361,7 +361,7 @@ BK_APP_WHITELIST = {}
 
 # 覆盖配置
 try:
-    from .base_bk import DATABASE_ROUTERS, TEMPLATES, STATICFILES_DIRS
+    from .base_bk import TEMPLATES, STATICFILES_DIRS
 except Exception:
     pass
 
