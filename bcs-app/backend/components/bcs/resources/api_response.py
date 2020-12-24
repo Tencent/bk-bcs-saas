0865df7254848013b26bb80763259ce70bd8ac25
raymondzhu <raymondzhu@tencent.com>
2020-12-24 19:18:53 +0800

    - 增加依赖 apiserver 的少量 resources 单元测试 - 重构 api_response.response 装饰器 - 修改 response 装饰器在出现普通 Exception 时，直接把异常放入 message   字段的问题

--- bcs-app/backend/components/bcs/resources/api_response.py
+++ bcs-app/backend/components/bcs/resources/api_response.py
@@ -14,52 +14,60 @@
 import json
 import logging
 from functools import wraps
+from typing import Callable, Dict, Any
 
 from kubernetes.client.rest import ApiException
 
 logger = logging.getLogger(__name__)
 
-DEFAULT_ERROR_CODE = 4001
-DEFAULT_SUCCESS_CODE = 0
 
-
-# 兼容view层的逻辑
-# 先前在view层是这样判断的
-# if not resp.get('code'):  raise xxx
 def response(format_data=True):
-    def decorator(func):
+    # TODO: format_data 参数现在已经没有作用了，因为 KubeResponseTransformer 的实现会自动判断，结果是否可以调用 to_dict 方法
+    # 如果可以的话，会自动调用，之后需要移除所有对该参数的使用。
+    return KubeResponseTransformer()
+
+
+# TODO: 这个响应转换属于更外层的逻辑，应该移动到更外层
+class KubeResponseTransformer:
+    """用于把可能返回 Kubernetes 客户端对象的函数响应，转换为可直接返回给前端的合法 JSON 响应"""
+
+    _default_error_code = 4001
+    _default_success_code = 0
+
+    def __call__(self, func: Callable):
         @wraps(func)
-        def _wrapped_func(*args, **kwargs):
+        def decorated(*args, **kwargs):
             try:
                 data = func(*args, **kwargs)
-                if format_data:
+                # 普通 Kubernetes 对象提供了 to_dict() 方法
+                if hasattr(data, 'to_dict'):
                     data = data.to_dict()
-                return {
-                    'code': DEFAULT_SUCCESS_CODE,
-                    'result': True,
-                    'data': data,
-                    'message': 'success'
-                }
+
+                return self.make_success_resp(data)
             except ApiException as err:
-                try:
-                    if err.status == 404:
-                        logger.info('resource not found, %s', err)
-                    else:
-                        logger.error('request bcs api error, %s', err)
-                    message = json.loads(err.body)['message']
-                except Exception:
-                    message = f'request bcs api error, {err}'
-                return {
-                    'code': DEFAULT_ERROR_CODE,
-                    'result': False,
-                    'message': message
-                }
+                if err.status == 404:
+                    logger.info('resource not found, %s', err)
+                else:
+                    logger.error('request bcs api error, %s', err)
+
+                error_msg = self.extract_message(err)
+                return self.make_error_resp(error_msg)
             except Exception as err:
                 logger.exception('request bcs api error, %s', err)
-                return {
-                    'code': DEFAULT_ERROR_CODE,
-                    'result': False,
-                    'message': err
-                }
-        return _wrapped_func
-    return decorator
+                return self.make_error_resp(str(err))
+
+        return decorated
+
+    @staticmethod
+    def extract_message(error: ApiException) -> str:
+        """从 ApiException 异常对象中尝试解析错误信息"""
+        try:
+            return json.loads(error.body)['message']
+        except Exception:
+            return f'request bcs api error, {error}'
+
+    def make_success_resp(self, data: Any) -> Dict:
+        return {'code': self._default_success_code, 'result': True, 'data': data, 'message': 'success'}
+
+    def make_error_resp(self, message: str) -> Dict:
+        return {'code': self._default_error_code, 'result': False, 'message': message}
