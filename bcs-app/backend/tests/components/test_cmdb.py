49ab96f1eaec7dd4350f3df321d494dd49aef582
bellkeyang <bellkeyang@tencent.com>
2021-01-06 11:16:42 +0800

    feat: update search cmdb biz

--- bcs-app/backend/tests/components/test_cmdb.py
+++ bcs-app/backend/tests/components/test_cmdb.py
@@ -5,12 +5,15 @@ from backend.components import cc
 
 
 @patch("backend.components.cc.get_all_application")
-def test_get_app_by_user_role(mock_get_all_application):
+def test_successful_get_app_by_user_role(mock_get_all_application):
     # 成功返回
     mock_get_all_application.return_value = {"code": 0, "data": [{"bk_biz_name": "test", "bk_biz_id": 1}]}
     biz_list = cc.get_app_by_user_role("admin")
     assert biz_list == [{"name": "test", "id": 1}]
 
+
+@patch("backend.components.cc.get_all_application")
+def test_failed_get_app_by_user_role(mock_get_all_application):
     # 异常返回
     mock_get_all_application.return_value = {"code": 400}
     biz_list = cc.get_app_by_user_role("admin")
