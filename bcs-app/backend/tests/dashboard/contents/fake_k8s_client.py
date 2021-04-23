ea638af0a90b2bce4b7e842fe7779f2b2b042a5c
schneesu <suzh9@mail2.sysu.edu.cn>
2021-04-23 17:29:24 +0800

    minor: workload resource list-watch api unittest.

---
+++ bcs-app/backend/tests/dashboard/contents/fake_k8s_client.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# Tencent is pleased to support the open source community by making 蓝鲸智云PaaS平台社区版 (BlueKing PaaS Community Edition) available.
+# Copyright (C) 2017-2019 THL A29 Limited, a Tencent company. All rights reserved.
+# Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://opensource.org/licenses/MIT
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+from functools import lru_cache
+import os
+
+from kubernetes import client
+
+from backend.resources.utils.discovery import BcsLazyDiscoverer, DiscovererCache
+from backend.resources.utils.kube_client import CoreDynamicClient
+
+
+class LocalKubeConfigurationService:
+    """指向本地集群的配置生成逻辑"""
+
+    def __init__(self):
+        self.api_key = os.environ.get('TESTING_SERVER_API_KEY')
+        self.host = os.environ.get('TESTING_API_SERVER_URL')
+
+    def make_configuration(self):
+        configuration = client.Configuration()
+        configuration.api_key = {'authorization': f"Bearer {self.api_key}"}
+        configuration.verify_ssl = False
+        configuration.host = self.host
+        return configuration
+
+
+@lru_cache(maxsize=128)
+def get_dynamic_client(access_token, project_id, cluster_id: str, *args, **kwargs) -> CoreDynamicClient:
+    """根据 token、cluster_id 等参数，构建访问 Kubernetes 集群的 Client 对象"""
+    config = LocalKubeConfigurationService().make_configuration()
+    discoverer_cache = DiscovererCache(cache_key=f"osrcp-{cluster_id}.json")
+    return CoreDynamicClient(client.ApiClient(config), cache_file=discoverer_cache, discoverer=BcsLazyDiscoverer)
---
+++ bcs-app/backend/tests/dashboard/workloads/test_cronjob.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#
+# Tencent is pleased to support the open source community by making 蓝鲸智云PaaS平台社区版 (BlueKing PaaS Community Edition) available.
+# Copyright (C) 2017-2019 THL A29 Limited, a Tencent company. All rights reserved.
+# Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://opensource.org/licenses/MIT
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+import pytest
+from unittest import mock
+
+from backend.tests.dashboard.contents.fake_viewsets import FakeSystemViewSet
+from backend.tests.dashboard.contents.fake_k8s_client import get_dynamic_client
+
+pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture(autouse=True)
+def common_patch():
+    with mock.patch('backend.dashboard.workload.views.cron_job.SystemViewSet', new=FakeSystemViewSet), \
+            mock.patch('backend.resources.resource.get_dynamic_client', new=get_dynamic_client):
+        yield
+
+
+class TestCronJob:
+
+    def test_list(self, api_client, project_id, cluster_id):
+        """ 测试获取资源列表接口 """
+        response = api_client.get(
+            f'/api/dashboard/projects/{project_id}/clusters/{cluster_id}/workload/cron_job/'
+        )
+        assert response.json()['code'] == 0
