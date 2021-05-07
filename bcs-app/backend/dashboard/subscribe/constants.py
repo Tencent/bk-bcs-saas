51d91f032237fbc5df341af0b43b69a57559dbb2
schneesu <suzh9@mail2.sysu.edu.cn>
2021-05-07 11:01:38 +0800

    minor: update constants.py.

--- bcs-app/backend/dashboard/subscribe/constants.py
+++ bcs-app/backend/dashboard/subscribe/constants.py
@@ -11,19 +11,20 @@
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 #
+from backend.resources.configurations.configmap import ConfigMap
+from backend.resources.configurations.secret import Secret
+from backend.resources.namespace.client import Namespace
+from backend.resources.networks.ingress import Ingress
+from backend.resources.networks.service import Service
+from backend.resources.storages.persistent_volume import PersistentVolume
+from backend.resources.storages.persistent_volume_claim import PersistentVolumeClaim
+from backend.resources.storages.storage_class import StorageClass
 from backend.resources.workloads.cronjob import CronJob
 from backend.resources.workloads.daemonset import DaemonSet
 from backend.resources.workloads.deployment import Deployment
 from backend.resources.workloads.job import Job
 from backend.resources.workloads.pod import Pod
 from backend.resources.workloads.statefulset import StatefulSet
-from backend.resources.networks.ingress import Ingress
-from backend.resources.networks.service import Service
-from backend.resources.storages.persistent_volume import PersistentVolume
-from backend.resources.storages.persistent_volume_claim import PersistentVolumeClaim
-from backend.resources.storages.storage_class import StorageClass
-from backend.resources.configurations.configmap import ConfigMap
-from backend.resources.configurations.secret import Secret
 
 # 默认监听时间：1s
 DEFAULT_SUBSCRIBE_TIMEOUT = 1
@@ -31,16 +32,25 @@ DEFAULT_SUBSCRIBE_TIMEOUT = 1
 # K8S Client
 K8S_RESOURCE_CLIENTS = [
     # workloads
-    CronJob, DaemonSet, Deployment, Job, Pod, StatefulSet,
+    CronJob,
+    DaemonSet,
+    Deployment,
+    Job,
+    Pod,
+    StatefulSet,
     # networks
-    Ingress, Service,
+    Ingress,
+    Service,
     # storages
-    PersistentVolume, PersistentVolumeClaim, StorageClass,
+    PersistentVolume,
+    PersistentVolumeClaim,
+    StorageClass,
     # configurations
-    ConfigMap, Secret
+    ConfigMap,
+    Secret,
+    # cluster
+    Namespace,
 ]
 
 # K8S资源类型：Client
-KIND_RESOURCE_CLIENT_MAP = {
-    client.kind: client for client in K8S_RESOURCE_CLIENTS
-}
+KIND_RESOURCE_CLIENT_MAP = {client.kind: client for client in K8S_RESOURCE_CLIENTS}
