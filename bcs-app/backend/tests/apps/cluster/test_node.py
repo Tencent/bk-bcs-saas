a8ae5a7c2894a0ced69ba650a166b1fbec7321bf
bellkeyang <bellkeyang@tencent.com>
2021-05-20 16:13:15 +0800

    feat: support batch or all node select

--- bcs-app/backend/tests/apps/cluster/test_node.py
+++ bcs-app/backend/tests/apps/cluster/test_node.py
@@ -15,26 +15,24 @@ import pytest
 from mock import patch
 
 from backend.apps.cluster.models import NodeStatus
-from backend.apps.cluster.serializers import BatchUpdateNodesSLZ
+from backend.apps.cluster.views.node import BatchUpdateDeleteNodeViewSet
 from backend.tests.bcs_mocks.data.paas_cc_json import fake_get_node_list_ok_resp
 from backend.tests.bcs_mocks.fake_bcs_cc import FakeBCSCCMod
+from backend.utils import FancyDict
 
 fake_cluster_node_id_list = [node["id"] for node in fake_get_node_list_ok_resp["results"]]
 
 
 @pytest.mark.parametrize(
-    "request_data,expect_node_id_list",
-    [
-        ({"status": NodeStatus.ToRemoved, "node_id_list": [1], "is_select_all": False}, [1]),
-        ({"status": NodeStatus.ToRemoved, "node_id_list": [1], "is_select_all": True}, [1]),
-        ({"status": NodeStatus.ToRemoved, "node_id_list": [], "is_select_all": True}, fake_cluster_node_id_list),
-    ],
+    "request_data,expected_node_id_list",
+    (FancyDict(data=FancyDict(status=NodeStatus.ToRemoved, node_id_list=[1], is_select_all=False)), [1]),
+    (FancyDict(data=FancyDict(status=NodeStatus.ToRemoved, node_id_list=[1], is_select_all=True)), [1]),
+    (
+        FancyDict(data=FancyDict(status=NodeStatus.ToRemoved, node_id_list=[], is_select_all=True)),
+        fake_cluster_node_id_list,
+    ),
 )
-@patch("backend.apps.cluster.serializers.paas_cc.PaaSCCClient", new=FakeBCSCCMod)
-def test_batch_node_slz(request_data, expect_node_id_list):
-    slz = BatchUpdateNodesSLZ(
-        data=request_data,
-        context={"access_token": "access_token", "project_id": "projectid", "cluster_id": "clusterid"},
-    )
-    slz.is_valid(raise_exception=True)
-    assert slz.validated_data["node_id_list"] == expect_node_id_list
+@patch("backend.resources.cluster.utils.paas_cc.PaaSCCClient", new=FakeBCSCCMod)
+def test_cluster_node_list(request_data, expected_node_id_list):
+    data = BatchUpdateDeleteNodeViewSet().get_request_params(request_data, "cluster_id")
+    assert data["node_id_list"] == expected_node_id_list
