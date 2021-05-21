31bf3e8e12dfd6933c0de490c233f369b7d436e4
bellkeyang <bellkeyang@tencent.com>
2021-05-21 20:16:50 +0800

    feat: 获取helm release note

--- bcs-app/backend/urls.py
+++ bcs-app/backend/urls.py
@@ -74,6 +74,10 @@ urlpatterns = [
         "api/logs/projects/<slug:project_id>/clusters/<slug:cluster_id>/",
         include("backend.container_service.observability.log_stream.urls"),
     ),
+    re_path(
+        r"^api/helm_releases/projects/(?P<project_id>\w{32})/clusters/(?P<cluster_id>[\w\-]+)/",
+        include("backend.bcs_k8s.releases.urls"),
+    ),
 ]
 
 # 导入版本特定的urls
