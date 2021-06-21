e4b8ca6343de5e81943bc4efc704a5023876301a
schneesu <suzh9@mail2.sysu.edu.cn>
2021-06-21 17:56:41 +0800

    feature: parse ingress rules.

--- bcs-app/backend/resources/networks/ingress/formatter.py
+++ bcs-app/backend/resources/networks/ingress/formatter.py
@@ -14,7 +14,7 @@
 from typing import Dict, List, Union
 
 from backend.resources.networks.common.formatter import NetworkFormatter
-from backend.utils.basic import getitems
+from backend.utils.basic import get_with_placeholder, getitems
 
 
 class IngressFormatter(NetworkFormatter):
@@ -42,6 +42,22 @@ class IngressFormatter(NetworkFormatter):
         """
         return '80, 443' if 'tls' in resource_dict['spec'] else '80'
 
+    def parse_rules(self, resource_dict: Dict) -> List:
+        rules = []
+        for r in getitems(resource_dict, 'spec.rules', []):
+            sub_rules = [
+                {
+                    'host': get_with_placeholder(r, 'host'),
+                    'path': get_with_placeholder(p, 'path'),
+                    'pathType': get_with_placeholder(p, 'pathType'),
+                    'serviceName': get_with_placeholder(p, 'backend.service.name'),
+                    'port': get_with_placeholder(p, 'backend.service.port.number'),
+                }
+                for p in getitems(r, 'http.paths', [])
+            ]
+            rules.extend(sub_rules)
+        return rules
+
     def format_dict(self, resource_dict: Dict) -> Dict:
         res = self.format_common_dict(resource_dict)
         res.update(
@@ -49,6 +65,7 @@ class IngressFormatter(NetworkFormatter):
                 'hosts': self.parse_hosts(resource_dict),
                 'addresses': self.parse_addresses(resource_dict),
                 'default_ports': self.parse_default_ports(resource_dict),
+                'rules': self.parse_rules(resource_dict),
             }
         )
         return res
