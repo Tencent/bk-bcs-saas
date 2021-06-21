b49df49f998e9a6dd99262b02bdd19c82aa514dc
HubuHito <1214105385@qq.com>
2021-06-21 21:13:27 +0800

    minor: 删除未使用文件

--- bcs-app/frontend/src/views/dashboard/workload/base-top-bar.tsx
+++
@@ -1,7 +0,0 @@
-import { defineComponent } from '@vue/composition-api'
-import DashboardTopActions from '../common/dashboard-top-actions'
-import './base-top-bar.css'
-
-export default defineComponent({
-
-})
