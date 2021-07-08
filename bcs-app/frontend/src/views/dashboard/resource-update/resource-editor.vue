50b53df394e9ac182faac50b41f345a17ce14197
HubuHito <1214105385@qq.com>
2021-07-08 17:00:05 +0800

    minor: 运行时加载worker

--- bcs-app/frontend/src/views/dashboard/resource-update/resource-editor.vue
+++ bcs-app/frontend/src/views/dashboard/resource-update/resource-editor.vue
@@ -9,6 +9,24 @@
     import yamljs from 'js-yaml'
     import BcsEditorTheme from './theme.json'
 
+    self.MonacoEnvironment = {
+        getWorkerUrl (moduleId, label) {
+            if (label === 'json') {
+                return `${window.DEVOPS_BCS_HOST}${window.STATIC_URL}${window.VERSION_STATIC_URL}/json.worker.js`
+            }
+            if (label === 'css') {
+                return `${window.DEVOPS_BCS_HOST}${window.STATIC_URL}${window.VERSION_STATIC_URL}/css.worker.js`
+            }
+            if (label === 'html') {
+                return `${window.DEVOPS_BCS_HOST}${window.STATIC_URL}${window.VERSION_STATIC_URL}/html.worker.js`
+            }
+            if (label === 'typescript' || label === 'javascript') {
+                return `${window.DEVOPS_BCS_HOST}${window.STATIC_URL}${window.VERSION_STATIC_URL}/ts.worker.js`
+            }
+            return `${window.DEVOPS_BCS_HOST}${window.STATIC_URL}${window.VERSION_STATIC_URL}/editor.worker.js`
+        }
+    }
+
     export default defineComponent({
         name: 'ResourceEditor',
         props: {
