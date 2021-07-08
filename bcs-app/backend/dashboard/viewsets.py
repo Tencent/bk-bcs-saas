3658301ce187dfbb8e570eff043714d2d6ca84e5
schnee <suzh9@mail2.sysu.edu.cn>
2021-07-08 16:52:09 +0800

    fix: Update resource (replace method) not require resourceVersion (#1166)
    * fix: Update resource (replace method) not require resourceVersion
    
    * minor: pop with default value

--- bcs-app/backend/dashboard/viewsets.py
+++ bcs-app/backend/dashboard/viewsets.py
@@ -85,9 +85,12 @@ class UpdateMixin:
         request.audit_ctx.update_fields(
             resource_type=self.resource_client.kind.lower(), resource=f'{namespace}/{name}'
         )
+        manifest = params['manifest']
+        # replace 模式下无需指定 resourceVersion
+        manifest['metadata'].pop('resourceVersion', None)
         try:
             response_data = client.replace(
-                body=params['manifest'], namespace=namespace, name=name, is_format=False
+                body=manifest, namespace=namespace, name=name, is_format=False
             ).data.to_dict()
         except DynamicApiError as e:
             raise UpdateResourceError(_('更新资源失败: {}').format(e.summary()))
