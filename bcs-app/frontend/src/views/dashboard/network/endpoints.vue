f520cf518c76336eabdc6dd0a05b7ef6372d70c4
HubuHito <1214105385@qq.com>
2021-07-14 14:52:17 +0800

    feature: 鉴权指令和资源视图鉴权逻辑

--- bcs-app/frontend/src/views/dashboard/network/endpoints.vue
+++ bcs-app/frontend/src/views/dashboard/network/endpoints.vue
@@ -1,6 +1,6 @@
 <template>
     <BaseLayout title="Endpoints" kind="Endpoints" category="endpoints" type="networks">
-        <template #default="{ curPageData, pageConf, handlePageChange, handlePageSizeChange, handleGetExtData, handleShowDetail, handleSortChange,handleUpdateResource,handleDeleteResource }">
+        <template #default="{ curPageData, pageConf, handlePageChange, handlePageSizeChange, handleGetExtData, handleShowDetail, handleSortChange,handleUpdateResource,handleDeleteResource, pagePerms }">
             <bk-table
                 :data="curPageData"
                 :pagination="pageConf"
@@ -25,8 +25,10 @@
                 </bk-table-column>
                 <bk-table-column :label="$t('操作')" :resizable="false" width="150">
                     <template #default="{ row }">
-                        <bk-button text @click="handleUpdateResource(row)">{{ $t('更新') }}</bk-button>
-                        <bk-button class="ml10" text @click="handleDeleteResource(row)">{{ $t('删除') }}</bk-button>
+                        <bk-button text v-authority="{ clickable: pagePerms.update.clickable, content: pagePerms.update.tip }"
+                            @click="handleUpdateResource(row)">{{ $t('更新') }}</bk-button>
+                        <bk-button class="ml10" text v-authority="{ clickable: pagePerms.delete.clickable, content: pagePerms.delete.tip }"
+                            @click="handleDeleteResource(row)">{{ $t('删除') }}</bk-button>
                     </template>
                 </bk-table-column>
             </bk-table>
