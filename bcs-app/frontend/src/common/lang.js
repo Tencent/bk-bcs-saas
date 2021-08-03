5f7e7ac62ce4fa5da91f3843392f2bd35cb5ec33
HubuHito <1214105385@qq.com>
2021-08-03 11:21:01 +0800

    fix: 修复私有化版本的测试问题

--- bcs-app/frontend/src/common/lang.js
+++ bcs-app/frontend/src/common/lang.js
@@ -1910,14 +1910,15 @@ export default {
     '等待所有Pod，PVC处于ready状态': ['Wait for all Pod and PVC to be ready'],
     '忽略CRD': ['Ignore the CRD'],
     '参数Key必须由 -- 字符开头': ['The parameter Key must begin with the -- character'],
-    '内容未变更或格式错误': [],
-    '确认退出当前编辑状态': [],
-    '退出后，你修改的内容将丢失': [],
-    '上传（仅支持YAML格式）': [],
-    '放大': [],
-    '示例': [],
-    '资源定义': [],
-    '存在时间': []
+    '内容未变更或格式错误': ['The content has not changed or the format is wrong'],
+    '确认退出当前编辑状态': ['Confirm to exit the current editing state'],
+    '退出后，你修改的内容将丢失': ['After you log out, your modified content will be lost'],
+    '上传（仅支持YAML格式）': ['Upload (only YAML format is supported'],
+    '放大': ['enlarge'],
+    '示例': ['Example'],
+    '资源定义': ['Resource definition'],
+    '存在时间': ['Existence time'],
+    '污点': ['Taint']
     // 变量的使用 $t('test', { vari1: 1, vari2: 2 })
     // // 变量的使用 $t('test', { vari1: 1, vari2: 2 })
     // 'test': ['{vari1} ---english--- {vari2}'],
