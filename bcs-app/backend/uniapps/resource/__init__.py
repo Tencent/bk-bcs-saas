902744fe4e54fb510890561765622c7f43f06a1e
schneesu <suzh9@mail2.sysu.edu.cn>
2021-08-04 22:28:13 +0800

    minor: pep8 E402

--- bcs-app/backend/uniapps/resource/__init__.py
+++ bcs-app/backend/uniapps/resource/__init__.py
@@ -11,5 +11,6 @@ You may obtain a copy of the License at
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
+
+管理统一应用资源，包括 ConfigMap、Secret 等
 """
-"""管理统一应用资源，包括 ConfigMap、Secret 等"""
