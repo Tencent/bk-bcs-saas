323c7b753cabd634b436972f8fbccc242e9cd050
vhwweng <1010382269@qq.com>
2021-08-18 14:44:48 +0800

    update：代码优化

--- bcs-app/frontend/src/views/cluster/node.vue
+++ bcs-app/frontend/src/views/cluster/node.vue
@@ -813,21 +813,23 @@
                                 </span>
                             </template>
                         </bk-table-column>
-                        <bk-table-column v-if="$INTERNAL" :label="$t('机房')" :show-overflow-tooltip="true">
-                            <template slot-scope="{ row }">
-                                {{row.idcunit || '--'}}
-                            </template>
-                        </bk-table-column>
-                        <bk-table-column v-if="$INTERNAL" :label="$t('机架')" :show-overflow-tooltip="true">
-                            <template slot-scope="{ row }">
-                                {{row.server_rack || '--'}}
-                            </template>
-                        </bk-table-column>
-                        <bk-table-column v-if="$INTERNAL" :label="$t('机型')" :show-overflow-tooltip="true">
-                            <template slot-scope="{ row }">
-                                {{row.device_class || '--'}}
-                            </template>
-                        </bk-table-column>
+                        <template v-if="$INTERNAL">
+                            <bk-table-column :label="$t('机房')" :show-overflow-tooltip="true">
+                                <template slot-scope="{ row }">
+                                    {{row.idcunit || '--'}}
+                                </template>
+                            </bk-table-column>
+                            <bk-table-column :label="$t('机架')" :show-overflow-tooltip="true">
+                                <template slot-scope="{ row }">
+                                    {{row.server_rack || '--'}}
+                                </template>
+                            </bk-table-column>
+                            <bk-table-column :label="$t('机型')" :show-overflow-tooltip="true">
+                                <template slot-scope="{ row }">
+                                    {{row.device_class || '--'}}
+                                </template>
+                            </bk-table-column>
+                        </template>
                         <div class="bk-message-box no-data" slot="empty">
                             <p class="message empty-message" v-if="ccSearchKeys.length">{{$t('无匹配的主机资源')}}</p>
                             <p class="message empty-message" v-else>{{$t('您在当前业务下没有主机资源，请联系业务运维')}}</p>
