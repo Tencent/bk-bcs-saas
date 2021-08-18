ab97b643d292d4b01a4c3e9f68ee54c595268654
Joe Lei <thezero12@hotmail.com>
2021-08-18 20:20:32 +0800

    add mesos redirect (#1366)

--- bcs-app/backend/urls.py
+++ bcs-app/backend/urls.py
@@ -5,9 +5,7 @@ Edition) available.
 Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
 Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://opensource.org/licenses/MIT
-
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
@@ -109,6 +107,7 @@ except ImportError:
 urlpatterns_vue = [
     # fallback to vue view
     url(r"^login_success.html", never_cache(LoginSuccessView.as_view())),
-    url(r"^.*$", never_cache(VueTemplateView.as_view())),
+    url(r"^(?P<project_code>[\w\-]+)/mesos", never_cache(VueTemplateView.as_view(container_orchestration="mesos"))),
+    url(r"^(?P<project_code>[\w\-]+)", never_cache(VueTemplateView.as_view(container_orchestration="k8s"))),
 ]
 urlpatterns += urlpatterns_vue
