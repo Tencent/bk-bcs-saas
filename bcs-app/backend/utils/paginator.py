b87fdd5d777334f289c18d6d61ec59cbe4aec867
schnee <suzh9@mail2.sysu.edu.cn>
2021-09-14 21:37:18 +0800

    refactor: 获取 CMDB Host 接口重构 & 性能优化 (#1443)
    * bugfix: fix issue #1301
    
    * perf: 重构获取可用 CC Host 接口，优化性能
    
    * perf: 重构获取可用 CC Host 接口，优化性能
    
    * perf: 重构获取可用 CC Host 接口，优化性能
    
    * perf: 拉取全量 Host 时候使用 async_run
    
    * minor: update comment, logic pref
    
    * minor: resolve #1443 conversations
    
    * test: patch all SystemViewSet & UserViewSet & get_dynamic_client
    
    * minor: resolve #1443 conversations
    
    * test: fix unittest no permission error

--- bcs-app/backend/utils/paginator.py
+++ bcs-app/backend/utils/paginator.py
@@ -14,6 +14,7 @@ specific language governing permissions and limitations under the License.
 """
 from collections import OrderedDict
 
+from django.core.paginator import Paginator
 from rest_framework.pagination import LimitOffsetPagination as _LimitOffsetPagination
 
 from backend.utils.response import APIResult
@@ -28,7 +29,6 @@ class LimitOffsetPagination(_LimitOffsetPagination):
         return super(LimitOffsetPagination, self).paginate_queryset(queryset, request, view=view)
 
     def get_paginated_response(self, data):
-
         data = OrderedDict(
             [
                 ('count', self.count),
@@ -40,3 +40,32 @@ class LimitOffsetPagination(_LimitOffsetPagination):
 
         # 按约定返回数据格式, message需要对象指定赋值
         return APIResult(data, message=getattr(self.view, 'message', ''))
+
+
+# 默认每页数量
+DEFAULT_PAGE_LIMIT = 5
+# 单次拉取最大上限
+MAX_LIMIT_COUNT = 200
+
+
+def custom_paginator(raw_data, offset, limit=None):
+    """使用django paginator进行分页处理"""
+    limit = limit or DEFAULT_PAGE_LIMIT
+
+    # 每页查询数量不可超过上限
+    if limit > MAX_LIMIT_COUNT:
+        limit = MAX_LIMIT_COUNT
+
+    page_cls = Paginator(raw_data, limit)
+    curr_page = 1
+    if offset or offset == 0:
+        curr_page = (offset // limit) + 1
+    # 如果当前页大于总页数，返回为空
+    count = page_cls.count
+    if curr_page > page_cls.num_pages:
+        return {"count": count, "results": []}
+
+    # 获取当前页的数据
+    curr_page_info = page_cls.page(curr_page)
+    curr_page_list = curr_page_info.object_list
+    return {"count": count, "results": curr_page_list}
