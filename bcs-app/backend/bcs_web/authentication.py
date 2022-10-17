60b9748acbcc62b2a9a03c877b29e559b28bd175
jamesgetx <gejun.coolfriend@gmail.com>
2022-10-17 20:27:10 +0800

    fix: missing BCS_AUTH_TOKEN in settings

--- bcs-app/backend/bcs_web/authentication.py
+++ bcs-app/backend/bcs_web/authentication.py
@@ -12,8 +12,6 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-from typing import Tuple
-
 from rest_framework import exceptions
 from rest_framework.authentication import BaseAuthentication
 
